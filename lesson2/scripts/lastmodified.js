2e607aa3531d4fecc31642a81e987f70a2c63630
BarkerEmily <emilybarker@byui.edu>
2023-01-14 14:39:04 -0700

    lesson2

---
+++ lesson2/scripts/lastmodified.js
@@ -0,0 +1,8 @@
+// Create lastUpdated
+function lastUpdated() {
+    let a = document.lastModified;
+    document.getElementById("lastmodified").innerHTML = a;
+}
+    
+    // Call lastUpdated
+    lastUpdated()
