b2dd2e6e6f5de4fbbae9c902472ebf8e552e50de
BarkerEmily <emilybarker@byui.edu>
2023-02-03 15:15:34 -0700

    main

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -1,30 +1,4 @@
-// Last Modified
-let currentDate = document.lastModified;
-document.querySelector('#lastModified').textContent = currentDate;
-
-// Year
-let date = new Date();
-let year = date.getFullYear();
-document.querySelector('#ear').innerHTML = year;
-
-// Today's Date
-const dateField = document.querySelector(".date");
-const fullDate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(date);
-dateField.innerHTML = fullDate;
-
-// Hamburger Menu
-let hamburger = document.querySelector('#hamburger');
-let nav = document.querySelector('header');
-
-
-
-//ham from lesson 3
-function toggleMenu() {
-    document.querySelector("#menu-close").classList.toggle('menu-active')
-    document.querySelector("#menu-open").classList.toggle('menu-active')
+var messagedate = new Data();
+if (messagedate.getDay() == 2 || messagedate.getDay() == 4) {
+    document.querySelector("#meet-greet").classList.add('active')
 }
-
-hamburger.addEventListener('click', () => {
-    nav.classList.toggle('responsive');
-
-});
