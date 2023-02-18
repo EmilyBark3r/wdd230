10048b0c9d3ce1222958c2a82363918db592a273
BarkerEmily <emilybarker@byui.edu>
2023-02-18 10:07:53 -0700

    changes

--- chamber/scripts/header-footer.js
+++ chamber/scripts/header-footer.js
@@ -1,6 +1,6 @@
 // This sets today's date in the header
 const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format( new Date() );
-document.querySelector(".date").textContent = fulldate;
+document.querySelector(".date p").textContent = fulldate;
 
 // This sets the year for the footer
 const today = new Date();
@@ -12,8 +12,8 @@ document.querySelector("#lastmodified").textContent = document.lastModified;
 // Toggle the menu open or closed
 function toggleMenu(){
     document.querySelector("nav ul").classList.toggle("menu-active");
-    document.querySelector("#hamburger-close").classList.toggle("menu-active");
-    document.querySelector("#hamburger-open").classList.toggle("menu-active");
+    document.querySelector("#hamburger-x").classList.toggle("menu-active");
+    document.querySelector("#hamburger-equiv").classList.toggle("menu-active");
 }
 
 // Attach click listener to the hamburger menu
--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,4 +1,22 @@
 // get the two input values of temperature and wind speed,
-// check to make sure they meet the specification limits  (<=50°F and >3.0mph) allowed to officially calculate the wind chill, and
-// either calculate and display the wind chill factor value or display "N/A" (not applicable) if the input values did not meet the requirements.
 // f = 35.74 + 0.6215t - 35.75s^0.16 + 0..4275ts^0.16
+function showWeather(temp,windspeed) {
+    temperatureobject = document.querySelector("#temperature");
+    windspeedobject = document.querySelector("windspeed");
+    windchillobject = document.querySelector("#windchill");
+    // check to make sure they meet the specification limits  (<=50°F and >3.0mph) allowed to officially calculate the wind chill
+    chillmsg = "N/A"
+    if(temp <= 50 && windspeed > 3) {
+        let chill = Math.round((35.74 + (0.6215 * temp) - (35.75 * Math.pow(windspeed, 0.16)+(0.4275 * temp)*(Math.pow(windspeed, 0.16)))))
+        chillmsg = `${chill}&deg; F`;
+    //                                                                        35.74 + 0.6215t - 35.75s^0.16 + 0.4275ts^0.16
+    // either calculate and display the wind chill factor value or display "N/A" (not applicable) if the input values did not meet the requirements.
+}
+
+temperatureobject.textContent = temp;
+windspeedobject.textContent = windspeed;
+windchillobject.innerHTML = chillmsg;
+
+}
+//test
+showWeather(7, 4);
