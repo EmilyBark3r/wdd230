ae147636711f1a7756ec3e0c146468b555be6c9e
BarkerEmily <emilybarker@byui.edu>
2023-03-01 15:29:24 -0700

    chamber

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,22 +1,22 @@
-// get the two input values of temperature and wind speed,
-// f = 35.74 + 0.6215t - 35.75s^0.16 + 0..4275ts^0.16
-function showWeather(temp,windspeed) {
-    temperatureobject = document.querySelector("#temperature");
-    windspeedobject = document.querySelector("windspeed");
-    windchillobject = document.querySelector("#windchill");
-    // check to make sure they meet the specification limits  (<=50°F and >3.0mph) allowed to officially calculate the wind chill
-    chillmsg = "N/A"
-    if(temp <= 50 && windspeed > 3) {
-        let chill = Math.round((35.74 + (0.6215 * temp) - (35.75 * Math.pow(windspeed, 0.16)+(0.4275 * temp)*(Math.pow(windspeed, 0.16)))))
-        chillmsg = `${chill}&deg; F`;
-    //                                                                        35.74 + 0.6215t - 35.75s^0.16 + 0.4275ts^0.16
-    // either calculate and display the wind chill factor value or display "N/A" (not applicable) if the input values did not meet the requirements.
+// // f = 35.74 + 0.6215t - 35.75s^0.16 + 0..4275ts^0.16
+
+function setWindChill(windspeed, temp){        
+    // Get the DOM objects that are dynamic
+    tempSpan = document.querySelector("#temperature");
+    windSpeedSpan = document.querySelector("#windspeed");
+    windChillSpan = document.querySelector("#windchill");
+
+    // Set up the wind chill content
+    let windchill = 'N/A';
+    if (windspeed >= 3.0 && temp <= 50){
+        let chill = Math.round((35.74 + (0.6215 * temp))-(35.75 * Math.pow(windspeed,0.16)) + (0.4275*temp*Math.pow(windspeed,0.16)));
+        windchill = `${chill}`;
+    }
+
+    // Write out the dynamic content
+    tempSpan.textContent = temp;
+    windSpeedSpan.textContent = windspeed;
+    windChillSpan.innerHTML = windchill;
 }
 
-temperatureobject.textContent = temp;
-windspeedobject.textContent = windspeed;
-windchillobject.innerHTML = chillmsg;
-
-}
-//test
-showWeather(7, 4);
+setWindChill(7, 5);
