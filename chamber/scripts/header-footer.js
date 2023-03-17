a9a7b5a1be2e608846f5a3f7f9171cb61463faad
BarkerEmily <emilybarker@byui.edu>
2023-03-16 20:13:12 -0600

    thanks

--- chamber/scripts/header-footer.js
+++ chamber/scripts/header-footer.js
@@ -1,6 +1,7 @@
 // This sets today's date in the header
-const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format( new Date() );
-document.querySelector(".header-today p").textContent = fulldate;
+const dateField = document.querySelector(".date");
+const fullDate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(date);
+dateField.innerHTML = fullDate;
 
 // Meeting announcement to appear on Mondays and Tuesdays
 const d = new Date();
@@ -15,5 +16,13 @@ if (day != 1 && day !=2){
 else{
 	meeting.style.display = "block";
 }
+
+// Copyright Date
+function year() {
+    let d = new Date();
+    let b = d.getFullYear();
+    document.getElementById("currentyear").innerHTML = b;
+}
+    year()
 
 
--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -2,18 +2,7 @@
 let currentDate = document.lastModified;
 document.querySelector('#lastModified').textContent = currentDate;
 
-// Copyright Date
-let date = new Date();
-let year = date.getFullYear();
-document.querySelector('#copyrightYear').innerHTML = year;
-
-// Today's Date
-const dateField = document.querySelector(".date");
-const fullDate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(date);
-dateField.innerHTML = fullDate;
-
 // Hamburger Menu
-
 let hamburger = document.querySelector('#hamburger');
 let nav = document.querySelector('header');
 
@@ -36,29 +25,4 @@ if (day == 1 || day == 2) {
     banner.style.display = 'none';
 }
 
-const images = document.querySelectorAll('img');
-const options = { threshold: 0.5, rootMargin: '0px 0px -100px 0px' };
-
-const preloadImage = (img) => {
-  const src = img.getAttribute('data-src');
-  if (!src) {
-    return;
-  }
-  img.src = src;
-}
-
-const io = new IntersectionObserver((entries, io) => {
-    entries.forEach(entry => {
-        if (!entry.isIntersecting) {
-            return;
-        } else {
-            preloadImage(entry.target);
-            io.unobserve(entry.target);
-        }
-    });
-}, options);
-
-images.forEach(image => {
-    io.observe(image);
-});
 
